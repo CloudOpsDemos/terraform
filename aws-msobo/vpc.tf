3a92196621f511686829d0f0a7c4c182cfcb78a1
Marek Sobolak <marek.sobolak@gmail.com>
2025-01-05 14:10:38 +0100

    Terr-4 Use default sg, nacl, route table for pub and priv subnets

--- aws-msobo/vpc.tf
+++ aws-msobo/vpc.tf
@@ -7,27 +7,6 @@ resource "aws_vpc" "k8s-vpc" {
   }
 }
 
-resource "aws_subnet" "k8s-sub-pub-2a" {
-  vpc_id                  = aws_vpc.k8s-vpc.id
-  cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-west-2a"
-  map_public_ip_on_launch = true
-
-  tags = {
-    Name = "${terraform.workspace}-k8s-sub-pub-1a"
-  }
-}
-
-resource "aws_subnet" "k8s-sub-priv-2b" {
-  vpc_id                  = aws_vpc.k8s-vpc.id
-  cidr_block              = "10.0.100.0/24"
-  availability_zone       = "us-west-2b"
-
-  tags = {
-    Name = "${terraform.workspace}-k8s-sub-priv-1b"
-  }
-}
-
 resource "aws_internet_gateway" "k8s-igw" {
   vpc_id = aws_vpc.k8s-vpc.id
 
@@ -36,8 +15,8 @@ resource "aws_internet_gateway" "k8s-igw" {
   }
 }
 
-resource "aws_route_table" "k8s-rt" {
-  vpc_id = aws_vpc.k8s-vpc.id
+resource "aws_default_route_table" "k8s-pub-rt" {
+  default_route_table_id = aws_vpc.k8s-vpc.default_route_table_id
 
   route {
     cidr_block = "0.0.0.0/0"
@@ -50,21 +29,121 @@ resource "aws_route_table" "k8s-rt" {
   }
 
   tags = {
-    Name = "k8s-rt"
+    Name = "${terraform.workspace}-k8s-pub-rt"
   }
 }
 
-resource "aws_route_table_association" "k8s-rta-pub-1a" {
-  subnet_id      = aws_subnet.k8s-sub-pub-2a.id
-  route_table_id = aws_route_table.k8s-rt.id
+resource "aws_route_table" "k8s-priv-rt" {
+  vpc_id = aws_vpc.k8s-vpc.id
+
+  route {
+    cidr_block = "10.0.0.0/16"
+    gateway_id = "local"
+  }
+
+  tags = {
+    Name = "${terraform.workspace}-k8s-priv-rt"
+  }
 }
 
-resource "aws_route_table_association" "k8s-rta-priv-1b" {
-  subnet_id      = aws_subnet.k8s-sub-priv-2b.id
-  route_table_id = aws_route_table.k8s-rt.id
+resource "aws_subnet" "k8s-pub-1" {
+  vpc_id            = aws_vpc.k8s-vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-west-2a"
+
+  tags = {
+    Name = "${terraform.workspace}-k8s-pub-1"
+  }
 }
 
-resource aws_main_route_table_association "k8s-rta-vpc" {
-  vpc_id         = aws_vpc.k8s-vpc.id
-  route_table_id = aws_route_table.k8s-rt.id
+resource "aws_subnet" "k8s-pub-2" {
+  vpc_id            = aws_vpc.k8s-vpc.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-west-2c"
+
+  tags = {
+    Name = "${terraform.workspace}-k8s-pub-2"
+  }
+}
+
+resource "aws_subnet" "k8s-priv-1" {
+  vpc_id            = aws_vpc.k8s-vpc.id
+  cidr_block        = "10.0.100.0/24"
+  availability_zone = "us-west-2b"
+
+  tags = {
+    Name = "${terraform.workspace}-k8s-priv-1"
+  }
+}
+
+resource "aws_subnet" "k8s-priv-2" {
+  vpc_id            = aws_vpc.k8s-vpc.id
+  cidr_block        = "10.0.101.0/24"
+  availability_zone = "us-west-2d"
+
+  tags = {
+    Name = "${terraform.workspace}-k8s-priv-2"
+  }
+}
+
+resource "aws_route_table_association" "k8s-pub" {
+  for_each = toset([aws_subnet.k8s-pub-1.id, aws_subnet.k8s-pub-2.id])
+
+  subnet_id = each.key
+  route_table_id = aws_vpc.k8s-vpc.default_route_table_id
+}
+
+resource "aws_route_table_association" "k8s-priv" {
+  for_each = toset([aws_subnet.k8s-priv-1.id, aws_subnet.k8s-priv-2.id])
+
+  subnet_id = each.key
+  route_table_id = aws_route_table.k8s-priv-rt.id
+}
+
+resource "aws_default_network_acl" "k8s-default-nacl" {
+  default_network_acl_id = aws_vpc.k8s-vpc.default_network_acl_id
+
+  ingress {
+    protocol   = -1
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 0
+  }
+
+  egress {
+    protocol   = -1
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 0
+  }
+
+  tags = {
+    Name = "${terraform.workspace}-k8s-default-nacl"
+  }
+}
+
+resource "aws_default_security_group" "k8s-default-sg" {
+  vpc_id = aws_vpc.k8s-vpc.id
+
+  ingress {
+    protocol  = -1
+    self      = true
+    from_port = 0
+    to_port   = 0
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${terraform.workspace}-k8s-default-sg"
+  }
 }
